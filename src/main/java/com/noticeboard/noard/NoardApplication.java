cc4f59d1bd1770ccdd10acc1a01e0c36bf162d6f
Choi Hong Gyu <zxolqs@gmail.com>
2023-12-28 16:40:04 +0900

    프로젝트 생성
    Co-authored-by: wonjinzz <gahwa518@naver.com>

---
+++ src/main/java/com/noticeboard/noard/NoardApplication.java
@@ -0,0 +1,13 @@
+package com.noticeboard.noard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noticeboard/noard/NoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticeboard.noard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
