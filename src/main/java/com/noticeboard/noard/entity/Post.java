f3f206f4f729d00ad35114330e207d4966f7ab57
wonjinzz <gahwa518@gmail.com>
2024-01-16 23:19:03 +0900

    [#1] feat: generate post, postImage entity

---
+++ src/main/java/com/noticeboard/noard/entity/Post.java
@@ -0,0 +1,57 @@
+package com.noticeboard.noard.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long postId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    @OneToMany(mappedBy = "post")
+    @JsonManagedReference
+    private List<PostImage> postImages = new ArrayList<>(); // post 여러 장
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public static Post createPost(String title, String content, List<String> imageUrls) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        for (String imageUrl : imageUrls) {
+            post.addPostImage(imageUrl);
+        }
+        return post;
+    }
+
+    public void addPostImage(String imagUrl) {
+        PostImage postImage = PostImage.createPostImage(imagUrl);
+        postImages.add(postImage);
+        postImage.setPost(this);
+    }
+
+//    public static Post getPost() {
+//        return getPost(this);
+//    }
+}
