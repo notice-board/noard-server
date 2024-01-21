ef832866d639e97802dd113655efe6ab48afa07d
wonjinzz <gahwa518@gmail.com>
2024-01-21 22:13:33 +0900

    [#1] chore: Refactor PostRepository to support both save and merge operations
    Co-authored-by: hong <zxolqs@gmail.com>

--- src/main/java/com/noticeboard/noard/repository/PostRepository.java
+++ src/main/java/com/noticeboard/noard/repository/PostRepository.java
@@ -16,14 +16,18 @@ public class PostRepository {
     private EntityManager em;
 
     public void save(Post post) {
-        em.persist(post); // 애초에 반환 타입이 없음.
+        if (post.getPostId() == null) {
+            em.persist(post); // 애초에 반환 타입이 없음.
+        } else {
+            em.merge(post);
+        }
     }
 
     public List<Post> findAll() {
         return em.createQuery("select p from Post p", Post.class).getResultList();
     }
 
-    public Post findOne(Long postId) {
+    public Post findOne(long postId) {
         return em.find(Post.class, postId);
     }
 
